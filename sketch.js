7f25bff8b99109e645de79755da1cdb9aaf28a78
adhya-agarwal <adhya.ag06@gmail.com>
2020-11-30 13:37:18 +0530

    added pig

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var pig5;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    pig5=new Pig(200,300);
 
 }
 
@@ -60,5 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    pig5.display();
     platform.display();
 }
